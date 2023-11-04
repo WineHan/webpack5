02ef7f9db4392ed6d883e96dbef3330ffaece01e
Henry Xie <hman821223@gmail.com>
2023-11-04 22:29:12 +0800

    Code Splitting End

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-import _ from "lodash";
-import printMe from "./print.js";
+// import _ from "lodash";
+// import printMe from "./print.js";
 
-function component() {
+// function component() {
+//   const element = document.createElement("div");
+//   const btn = document.createElement("button");
+
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+//   btn.innerHTML = "Click me and check the console!";
+//   btn.onclick = printMe;
+
+//   element.appendChild(btn);
+
+//   return element;
+// }
+
+const btn = document.createElement("button");
+
+btn.innerHTML = "Click me dynamic import!";
+
+document.body.appendChild(btn);
+
+btn.onclick = async function getComponent() {
   const element = document.createElement("div");
-  const btn = document.createElement("button");
+  const { default: _ } = await import("lodash");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
   return element;
-}
+};
 
-document.body.appendChild(component());
+// getComponent().then((component) => {
+//   document.body.appendChild(component);
+// });
