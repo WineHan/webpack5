0598ae3db97511e93b7154b2199a5d3ec4ee3653
Henry Xie <hman821223@gmail.com>
2023-11-04 14:59:24 +0800

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import _ from "lodash";
+import "./style.css";
+import MyImage from "./one.JPG";
+import Data from "./data.xml";
+import Notes from "./data.csv";
+
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = MyImage;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
